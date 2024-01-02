f3dde42ede4dc4a36fcc695d37ef729468ad75c4
Daros <marcelo.sdaros@gmail.com>
2024-01-01 22:23:10 -0300

    adding features

--- src/components/body/play/Play.js
+++ src/components/body/play/Play.js
@@ -1,41 +1,12 @@
-import { useState } from 'react'
 import style from './Play.module.css'
 import Block from './block/Block'
 
-const Play = ({ description, changeScreen }) => {
-
-  const terms = [
-    {tip: "computer", words: ["KEYBOARD", "MOUSE", "HEADSET", "SOFTWARE", "HARDWARE", "WINDOWS"]},
-    {tip: "vehicle", words: ["CAR", "HELICOPTER", "AIRPLANE", "BOAT", "BUS", "BICICLE"]},
-    {tip: "food", words: ["PASTA", "PIZZA", "SUSHI", "BARBECUE", "SPINACH", "WATERMELON"]}
-  ]
-  
-  const [attempts, setAttempts] = useState(3)
-  const [usedLetters, setUsedLetters] = useState([])
-  let numTip = Math.floor(Math.random()*3)
-  let numWord = Math.floor(Math.random()*6)
-  const tip = terms[numTip].tip
-  const word = terms[numTip].words[numWord]
-  const letters = word.split('')
-  console.log(word)
+const Play = ({ description, changeScreen, tip, attempts, letters, usedLetters, handleSubmit }) => {
 
   const validateInput = (e) => {
     e.target.value = 
     e.target.value
       .replace(/\W|\d/g, '').substr(0, 1).toUpperCase();
-  }
-
-  const handleSubmit = (event) => {
-    event.preventDefault()
-
-    const character = document.querySelector('#letter').value
-    const index = letters.findIndex((element) => element === character)
-    if (index !== -1) {
-      document.getElementById(index).innerHTML = character
-    }
-    else {
-      setUsedLetters(usedLetters => [...usedLetters, character])
-    }
   }
 
   return (
@@ -45,16 +16,17 @@ const Play = ({ description, changeScreen }) => {
       <div className={style.block_area}>
         {letters.map((character, index) => (
           <Block 
+            key={index}
             index={index}
           />
         ))}
       </div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           <p className={style.text}>Tente adivinhar uma letra da palavra:</p>
           <p className={style.input_wrapper}>
             <input type="text" id="letter" className={style.input} onChange={validateInput}/>
-            <button className={style.btn} onClick={handleSubmit}>JOGAR!</button>
+            <input type="submit" className={style.btn} value="JOGAR!"/>
           </p>
         </label>
       </form>
--- src/components/body/play/block/Block.js
+++ src/components/body/play/block/Block.js
@@ -1,7 +1,6 @@
 import style from './Block.module.css'
 
 const Block = ({ index }) => {
-  console.log(index)
   return (
     <div id={index} className={style.block}></div>
   )
